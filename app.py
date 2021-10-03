820bc740457898304c4b9ba02850855b1db75bcf
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-10-03 21:47:07 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,129 @@
+from flask import Flask, redirect, render_template, request, session, flash
+from datetime import timedelta
+from functions import apology, auth, get_user_data, get_user_guilds, get_channels
+from functools import wraps
+
+
+
+# Configure application
+app = Flask(__name__)
+
+
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SECRET_KEY'] = "@#$@$#123$FDGFDT$%%Rkr ewr ewrefdsqwe2 s"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)
+app.url_map.strict_slashes = False
+
+users = {}
+a_m_guilds = {}
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None or users.get(session["user_id"]) is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def index():
+    if request.method == "GET":
+        return render_template("index.html", users=users, servers=a_m_guilds[session["user_id"]])
+    
+    if request.method == "POST":
+        if request.form.get("server") is None:
+            return apology("You didn't select any server", users)
+        else:
+            serverId = request.form.get("server")
+    return redirect(f"/dashboard/{serverId}")
+
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    if request.method == "POST":
+        return redirect("https://discord.com/api/oauth2/authorize?client_id=879273611603619881&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Floggedin&response_type=code&scope=identify%20guilds")
+
+@app.route('/loggedin')
+def loggedin():
+    auth_key = {}
+    code = request.args.get("code")
+    if not code:
+        session.clear()
+        return apology("No codes provided! please use login button", users)
+    else:
+        auth_key = auth(code)
+        if auth_key == "ERROR: 1":
+            session.clear()
+            return apology("Couldn't get auth token. Please re-login", users)
+
+    user = get_user_data(auth_key)
+    if user == "ERROR: 2":
+        session.clear()
+        return apology("Couldn't get user data. Please re-login", users)
+
+    session["user_id"] = user["id"]
+    users[user["id"]] = user
+    
+
+    user_guilds = get_user_guilds(auth_key)
+    if user_guilds == "ERROR: 3":
+        return apology("Couldn't get user_guilds data. Please re-login", True)
+
+    a_m_guilds[session["user_id"]] = user_guilds
+
+    return redirect('/')
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/dashboard/<server_id>", methods=["GET", "POST"])
+@login_required
+def dashboard(server_id):
+    if request.method == "GET":
+        if a_m_guilds[session["user_id"]].get(server_id) is None:
+            return apology("Couldn't open this server dashboard.!\nDo you have [MANAGE_GUILD] permission in this server ?", users=users)
+        else:
+
+            channels = get_channels(server_id)
+            server_name = a_m_guilds[session["user_id"]][server_id]["name"]
+            return render_template("dashboard.html", users=users, channels=channels, server_name=server_name)
+    
+    if request.method == "POST":
+        flash("This future is OFF now!")
+        return redirect(request.url)
+    
+
+
+@app.route("/dashboard")
+def dashboard1():
+    return redirect("/")
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,122 @@
+from flask import redirect, render_template, request, session
+import requests
+
+
+def apology(message, users):
+    return render_template("error.html", message=message, users=users)
+
+
+
+url = 'https://discord.com/api/oauth2/token'
+CLIENT_ID = '879273611603619881'
+CLIENT_SECRET = 'HNvZD6ymlx8b5-BFtLvv8LTihQT8CNQw'
+REDIRECT_URI = 'http://localhost:5000/loggedin'
+bot_token = "ODc5MjczNjExNjAzNjE5ODgx.YSNVXQ.KiKNaFWWQRvq0Q5zNLy_b7Z41ZA"
+
+def auth(code):
+
+    data = {
+      'client_id': CLIENT_ID,
+      'client_secret': CLIENT_SECRET,
+      'grant_type': 'authorization_code',
+      'code': code,
+      'redirect_uri': REDIRECT_URI
+    }
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    auth_data = ''
+    try:
+        r = requests.post(url, data=data, headers=headers)
+        r.raise_for_status()
+        auth_data = r.json()
+    except:
+        return "ERROR: 1"
+    return(f'{auth_data["token_type"]} {auth_data["access_token"]}')
+
+    
+    
+    
+def get_user_data(auth_key):
+
+    
+    headers = {
+       'authorization': auth_key
+      }
+   
+    try:
+        r = requests.get("https://discord.com/api/users/@me", headers=headers)
+        r.raise_for_status()
+        return r.json()
+    except:
+        return "ERROR: 2"
+
+def get_user_guilds(auth_key):
+    headers = {
+       'authorization': auth_key
+      }
+
+    all_guilds = {}
+    try:
+        r = requests.get("https://discord.com/api/users/@me/guilds", headers=headers)
+        r.raise_for_status()
+        all_guilds = r.json()
+    except:
+        return "ERROR: 3"
+
+    a_m_guilds = admin_mutual_guilds(all_guilds)
+
+    return a_m_guilds
+
+        
+    
+def admin_mutual_guilds(guilds):
+    headers = {
+       'authorization': f"Bot {bot_token}"
+      }
+
+    mutual_guilds = []
+
+    for guild in guilds:
+        try:
+            r = requests.get(f"https://discord.com/api/guilds/{guild['id']}", headers=headers)
+            r.raise_for_status()
+            mutual_guilds.append(guild)
+        except:
+            pass
+    
+    admin_guilds = {}
+
+    for guild in mutual_guilds:
+        if (guild['permissions'] & 0x8) == 0x8:
+            admin_guilds[guild['id']] = guild
+
+    return admin_guilds
+
+
+
+
+def get_channels(server_id):
+    headers = {
+       'authorization': f"Bot {bot_token}"
+    }
+
+    channels = {}
+    text_channels = {}
+
+    try:
+        r = requests.get(f"https://discord.com/api/guilds/{server_id}/channels", headers=headers)
+        r.raise_for_status()
+        channels = r.json()
+    except:
+        pass
+
+    for channel in channels:
+        if channel["type"] == 0:
+            text_channels[channel["id"]] = channel
+
+    return text_channels
+            
+
+
+
