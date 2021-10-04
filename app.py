2470e68ef4fd4bab6f5debe8820e5ff9ae296675
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-10-04 11:18:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, redirect, render_template, request, session, flash
 from datetime import timedelta
 from functions import apology, auth, get_user_data, get_user_guilds, get_channels
 from functools import wraps
+from flask_session import Session
+
 
 
 
@@ -11,13 +13,9 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SECRET_KEY'] = "@#$@$#123$FDGFDT$%%Rkr ewr ewrefdsqwe2 s"
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)
-app.config["SESSION_TYPE"] = "filesystem"
 app.url_map.strict_slashes = False
 
 
-
-users = {}
-a_m_guilds = {}
 
 def login_required(f):
     """
@@ -27,7 +25,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None or users.get(session["user_id"]) is None:
+        if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
@@ -37,11 +35,11 @@ def login_required(f):
 @login_required
 def index():
     if request.method == "GET":
-        return render_template("index.html", users=users, servers=a_m_guilds[session["user_id"]])
+        return render_template("index.html", user=session["user"], servers=session["guilds"])
     
     if request.method == "POST":
         if request.form.get("server") is None:
-            return apology("You didn't select any server", users)
+            return apology("You didn't select any server", session["user"])
         else:
             serverId = request.form.get("server")
     return redirect(f"/dashboard/{serverId}")
@@ -56,7 +54,7 @@ def login():
         return render_template("login.html")
     
     if request.method == "POST":
-        return redirect("https://discord.com/api/oauth2/authorize?client_id=879273611603619881&redirect_uri=https%3A%2F%2Fchat-log-dashboard.herokuapp.com%2Floggedin&response_type=code&scope=identify%20guilds")
+        return redirect("https://discord.com/api/oauth2/authorize?client_id=879273611603619881&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Floggedin&response_type=code&scope=identify%20guilds")
 
 @app.route('/loggedin')
 def loggedin():
@@ -64,28 +62,27 @@ def loggedin():
     code = request.args.get("code")
     if not code:
         session.clear()
-        return apology("No codes provided! please use login button", users)
+        return apology("No codes provided! please use login button", session["user"])
     else:
         auth_key = auth(code)
         if auth_key == "ERROR: 1":
             session.clear()
-            return apology("Couldn't get auth token. Please re-login", users)
+            return apology("Couldn't get auth token. Please re-login", session["user"])
 
     user = get_user_data(auth_key)
     if user == "ERROR: 2":
         session.clear()
-        return apology("Couldn't get user data. Please re-login", users)
+        return apology("Couldn't get user data. Please re-login", session["user"])
 
     session["user_id"] = user["id"]
-    users[user["id"]] = user
+    session["user"] = user
     
 
     user_guilds = get_user_guilds(auth_key)
     if user_guilds == "ERROR: 3":
         return apology("Couldn't get user_guilds data. Please re-login", True)
 
-    a_m_guilds[session["user_id"]] = user_guilds
-
+    session["guilds"] = user_guilds
     return redirect('/')
 
 @app.route("/logout")
@@ -103,13 +100,13 @@ def logout():
 @login_required
 def dashboard(server_id):
     if request.method == "GET":
-        if a_m_guilds[session["user_id"]].get(server_id) is None:
-            return apology("Couldn't open this server dashboard.!\nDo you have [MANAGE_GUILD] permission in this server ?", users=users)
+        if session["guilds"].get(server_id) is None:
+            return apology("Couldn't open this server dashboard.!\nDo you have [MANAGE_GUILD] permission in this server ?", user=session["user"])
         else:
 
             channels = get_channels(server_id)
-            server_name = a_m_guilds[session["user_id"]][server_id]["name"]
-            return render_template("dashboard.html", users=users, channels=channels, server_name=server_name)
+            server_name = session["guilds"][server_id]["name"]
+            return render_template("dashboard.html", user=session["user"], channels=channels, server_name=server_name)
     
     if request.method == "POST":
         flash("This future is OFF now!")
